0899135c1586faa4f19138032ed18e56724cbfcc
Raju Mannem <rajumannem71@gmail.com>
2024-06-13 03:01:43 +0530

    configure professor webapp

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app=express();
+app.use(express.static("public"));
+app.get('/',(req,res)=>{
+    res.render("index.ejs")
+})
+app.get('/login',(req,res)=>{
+    res.render("login.ejs")
+})
+app.listen(3000,()=>{
+    console.log('server is  running on 3000')
+});
